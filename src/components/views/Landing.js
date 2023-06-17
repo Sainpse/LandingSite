59dc587ad028286a9552e4aafd058165fd67ae31
unknown <marcus@darkpools.co.za>
2023-06-17 14:49:54 +0200

    Infinite load times on mobile fix attempt

--- src/components/views/Landing.js
+++ src/components/views/Landing.js
@@ -1,30 +1,19 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import { Container} from 'rsuite';
 import Navigation from '../elements/Navigation';
 import HeaderSainpse from "../elements/HeaderSainpse";
-import Loader from '../elements/Loader'
-import "../../styles/Landing.css";
+
 
 
 
 const Landing = () => {
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        window.onload = () => {
-        setIsLoading(false);
-        };
-    }, []);
-
 
     return (
         <div className="show-fake-browser sidebar-page header">
             <Container className='Landingbg'>
                 <Navigation />
                 <Container className='bg'>
-                {isLoading ? <Loader /> : <HeaderSainpse />}
-                   
+                    <HeaderSainpse />
                 </Container>
             </Container>
         </div>
